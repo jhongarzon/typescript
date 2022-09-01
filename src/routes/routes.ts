0d695a3f81b0698fa1f83e4b49ef0ed64cdd0a3f
jhon <johngarzon86@gmail.com>
2022-08-31 23:23:55 -0500

    Feat: Basic lazy load components

---
+++ src/routes/routes.ts
@@ -0,0 +1,33 @@
+import { LazyPage1 } from "../01-lazyload/pages";
+import React, { lazy, LazyExoticComponent } from "react";
+interface Route {
+  to: string;
+  path: string;
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent;
+  name: string;
+}
+type JSXComponent = () => JSX.Element;
+const Lazy1 = lazy(() => import("../01-lazyload/pages/LazyPage1"));
+const Lazy2 = lazy(() => import("../01-lazyload/pages/LazyPage2"));
+const Lazy3 = lazy(() => import("../01-lazyload/pages/LazyPage3"));
+
+export const routes: Route[] = [
+  {
+    to: "/lazy1",
+    path: "lazy1",
+    Component: Lazy1,
+    name: "Lazy-1",
+  },
+  {
+    to: "/lazy2",
+    path: "lazy2",
+    Component: Lazy2,
+    name: "Lazy-2",
+  },
+  {
+    to: "/lazy3",
+    path: "lazy3",
+    Component: Lazy3,
+    name: "Lazy-3",
+  },
+];
