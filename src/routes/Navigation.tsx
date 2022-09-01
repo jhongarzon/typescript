480ae70da736b471cb8f045b8413c42d5c7d88c3
Jhon Garzon <jgarzon@ueat.io>
2022-08-31 21:51:18 -0500

    Initial commit

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,55 @@
+import {
+  BrowserRouter,
+  Navigate,
+  NavLink,
+  Route,
+  Routes,
+} from "react-router-dom";
+import logo from "../assets/react.svg";
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout"></div>
+      <nav>
+        <img src={logo}></img>
+        <ul>
+          <li>
+            <NavLink
+              to="/home"
+              className={({ isActive }) => (isActive ? "nav-active" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? "nav-active" : "")}
+            >
+              {" "}
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/users"
+              className={({ isActive }) => (isActive ? "nav-active" : "")}
+            >
+              {" "}
+              Users
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="about" element={<h1>About</h1>}></Route>
+        <Route path="users" element={<h1>Users</h1>}></Route>
+        <Route path="home" element={<h1>Home</h1>}></Route>
+        <Route
+          path="/*"
+          element={<Navigate to="/home" replace></Navigate>}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
